901ab7d588f57261e88b2128c2616aa856bdbcd4
mrFlick72 <valerio.vaudi@gmail.com>
2018-12-12 09:05:48 +0100

    properties instead of hard coded config and clean up useless code

--- src/main/kotlin/it/valeriovaudi/lab/reservationservice/ReservationServiceApplication.kt
+++ src/main/kotlin/it/valeriovaudi/lab/reservationservice/ReservationServiceApplication.kt
@@ -1,34 +1,11 @@
 package it.valeriovaudi.lab.reservationservice
 
-import it.valeriovaudi.lab.reservationservice.domain.model.Customer
-import it.valeriovaudi.lab.reservationservice.domain.repository.CustomerRepository
-import org.springframework.boot.ApplicationArguments
-import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Component
-import reactor.core.publisher.toMono
-import java.util.*
 
 @SpringBootApplication
 class ReservationServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<ReservationServiceApplication>(*args)
-}
-
-@Component
-class Init(private val customerRepositoy: CustomerRepository) : ApplicationRunner {
-
-    override fun run(args: ApplicationArguments?) {
-        println("insert")
-        customerRepositoy.save(Customer(UUID.randomUUID().toString(), "Valerio", "Vaudo"))
-                .toMono().subscribe({ println(it)})
-
-   /*     println("findAll")
-        customerRepositoy.findAll().toFlux().subscribe({ println(it)})*/
-
-
-    }
-
 }
